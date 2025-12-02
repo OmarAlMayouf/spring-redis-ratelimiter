47c9aa6b0bff78a2429e0bbdf3b4faf69213b906
Omar Al Mayouf <o.almayouf@takamol.sa>
2025-12-02 03:35:21 +0300

    initial commit

---
+++ src/main/java/io/github/omaralmayouf/springredisratelimiter/SpringRedisRatelimiterApplication.java
@@ -0,0 +1,13 @@
+package io.github.omaralmayouf.springredisratelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisRatelimiterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisRatelimiterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/omaralmayouf/springredisratelimiter/SpringRedisRatelimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.omaralmayouf.springredisratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisRatelimiterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
